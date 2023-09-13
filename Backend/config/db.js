89754861f29390c2aa76d7640c5a7ff59efef9a2
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2023-09-13 23:19:29 +0530

    db added

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 mongoose.set('strictQuery', false);
 
 module.exports = connectDB => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log("Connected to MongoDB 🔥 ".cyan.bold))
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'HashedSecrets', })
+        .then(() => console.log(`Connected to MongoDB database: 🔥 `.cyan.bold))
         .catch((err) => console.log(`${err} 😫`.red.bold))
 }
